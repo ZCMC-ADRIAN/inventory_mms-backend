e5ed6f34b911ea821696395a33add5df8bcc43c0
dennis falcasantos <dennisfalcasantosnatividad@gmail.com>
2023-01-31 16:54:31 +0800

    partially done

--- routes/api.php
+++ routes/api.php
@@ -34,8 +34,10 @@ Route::namespace('App\Http\Controllers')->group(function () {
     Route::GET('/item', 'ItemController@index');
     Route::GET('/itemdetail/{id}', 'ItemController@query');
     
+    
     Route::GET('/location', 'LocationController@index');
     Route::GET('/condition', 'ConditionController@query');
+
     //
     Route::GET('/type', 'TypesController@index');
     Route::GET('/brand', 'BrandController@index');
