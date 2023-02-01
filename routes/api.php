9d98ae56807c4a84732d1c2fe4a873829cb66761
dennis falcasantos <dennisfalcasantosnatividad@gmail.com>
2023-02-01 15:54:47 +0800

    complete route modified

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,24 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\SignUp;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/signup', [SignUp::class, 'SignUp']);
+Route::post('/signin', [SignUp::class, 'Login']);
 
-Route::namespace('App\Http\Controllers')->group(function () {
-    // Route::post('/details', 'InsertDetails@insert_details');
-    Route::post('/create', 'CreateItem@CreateItem');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::namespace('App\Http\Controllers')->group(function () {
+        Route::post('/logout', 'SignUp@logout');
+         Route::post('/create', 'CreateItem@CreateItem');
 
     //Fields
+    Route::get('/user', 'UserDetails@user_details');
     Route::get('/article', 'GetData\Fields@get_article');
     Route::get('/types', 'GetData\Fields@get_types');
     Route::get('/status', 'GetData\Fields@get_status');
@@ -47,7 +43,7 @@ Route::namespace('App\Http\Controllers')->group(function () {
     Route::get('/data-table', 'GetData\Table@data_table');
     Route::get('/available', 'GetData\Table@available');
     Route::get('/details', 'GetData\Table@details');
-
-
-    // Route::get('/test', 'test@test');
+    });
 });
+        
+
