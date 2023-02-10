ebd026e7dd57bbbd47f725e858d58e4b29788372
ZCMC-ADRIAN <adrian.zcmc@gmail.com>
2023-02-10 16:42:38 +0800

    'newest'

--- app/Http/Controllers/GetData/Table.php
+++ app/Http/Controllers/GetData/Table.php
@@ -8,45 +8,26 @@ use Illuminate\Support\Facades\DB;
 
 class Table extends Controller
 {
-    public function data_table()
+    public function data_table(Request $req)
     {
         try {
-            $table = DB::table('inventories')
-                ->join('items', 'inventories.Fk_itemId', '=', 'items.Pk_itemId')
-                ->join('types', 'items.Fk_typeId', '=', 'types.Pk_typeId')
-                ->join('articles', 'types.Fk_articleId', '=', 'articles.Pk_articleId')
-                ->leftJoin('variety', 'variety.Pk_varietyId', '=', 'items.Fk_varietyId')
-                ->select('*', DB::raw('CONCAT_WS(" ", article_name, type_name, model, variety, details2) AS "desc"'))
-                ->get();
 
-            return response()->json($table);
-            
-        } catch (\Throwable $th) {
-            return response()->json([
-                'message' => $th
-            ]);
-        }
-    }
+            if ($req->has('q')) {
+                $q = $req->input('q');
+                $table = DB::select('SELECT location_name, Quantity, `serial`, property_no,  CONCAT_WS(" ", article_name, type_name, model, variety, details2) AS "desc" from inventories INNER JOIN items ON inventories.Fk_itemId = items.Pk_itemId INNER JOIN locat_man ON inventories.Fk_locatmanId = locat_man.Pk_locatmanId INNER JOIN location ON locat_man.Fk_locationId = location.Pk_locationId INNER JOIN types ON items.Fk_typeId = types.Pk_typeId INNER JOIN articles ON types.Fk_articleId = articles.Pk_articleId INNER JOIN variety ON items.Fk_varietyId = variety.Pk_varietyId WHERE location_name LIKE ?', ["%$q%"]);
 
-    public function details(Request $req){
-        try {
-            $detail = DB::table('inventories')
+                return response()->json($table);
+            } else {
+                $table = DB::table('inventories')
                     ->join('items', 'inventories.Fk_itemId', '=', 'items.Pk_itemId')
                     ->join('types', 'items.Fk_typeId', '=', 'types.Pk_typeId')
                     ->join('articles', 'types.Fk_articleId', '=', 'articles.Pk_articleId')
                     ->leftJoin('variety', 'variety.Pk_varietyId', '=', 'items.Fk_varietyId')
-                    ->leftJoin('brands', 'brands.Pk_brandId', '=', 'items.Fk_brandId')
-                    ->leftJoin('manufacturers', 'manufacturers.Pk_manuId', '=', 'items.Fk_manuId')
-                    ->leftJoin('suppliers', 'manufacturers.Pk_manuId', '=', 'items.Fk_manuId')
-                    ->leftJoin('countries', 'countries.Pk_countryId', '=', 'items.Fk_countryId')
-                    ->leftJoin('associate', 'associate.Pk_assocId', '=', 'inventories.Fk_assocId')
-                    ->leftJoin('location', 'location.Pk_locationId', '=', 'associate.Fk_locationId')
-                    ->select('*', DB::raw('FORMAT(cost,2) as costs'), DB::raw('Quantity * pack_size + loose as qty'))
-                    ->where('Pk_inventoryId', $req->inId)
+                    ->select('*', DB::raw('CONCAT_WS(" ", article_name, type_name, model, variety, details2) AS "desc"'))
                     ->get();
-                    
-            return response()->json($detail);
 
+                return response()->json($table);
+            }
         } catch (\Throwable $th) {
             return response()->json([
                 'message' => $th
@@ -54,25 +35,29 @@ class Table extends Controller
         }
     }
 
-    // public function items(Request $req){
-    //     try{
-    //         $items = DB::table('inventories')
-    //                 ->join('items', 'inventories.Fk_itemId', '=', 'items.Pk_itemId')
-    //                 ->join('locat_man', 'inventories.Fk_locatmanId', '=', 'locat_man.Pk_locatmanId')
-    //                 ->join('location', 'locat_man.Fk_locationId', '=', 'location.Pk_locationId')
-    //                 ->join('types', 'items.Fk_typeId', '=', 'types.Pk_typeId')
-    //                 ->join('articles', 'types.Fk_articleId', '=', 'articles.Pk_articleId')
-    //                 ->join('variety', 'items.Fk_varietyId', '=', 'variety.Pk_varietyId')
-    //                 ->select('location_name', 'Quantity',DB::raw('CONCAT_WS(" ", article_name, type_name, model, variety, details2) AS "desc"'))
-    //                 ->where(DB::raw('CONCAT_WS(" ", article_name, type_name, model, variety, details2)'), $req->desc)
-    //                 ->get();
+    public function details(Request $req)
+    {
+        try {
+            $detail = DB::table('inventories')
+                ->join('items', 'inventories.Fk_itemId', '=', 'items.Pk_itemId')
+                ->join('types', 'items.Fk_typeId', '=', 'types.Pk_typeId')
+                ->join('articles', 'types.Fk_articleId', '=', 'articles.Pk_articleId')
+                ->leftJoin('variety', 'variety.Pk_varietyId', '=', 'items.Fk_varietyId')
+                ->leftJoin('brands', 'brands.Pk_brandId', '=', 'items.Fk_brandId')
+                ->leftJoin('manufacturers', 'manufacturers.Pk_manuId', '=', 'items.Fk_manuId')
+                ->leftJoin('suppliers', 'manufacturers.Pk_manuId', '=', 'items.Fk_manuId')
+                ->leftJoin('countries', 'countries.Pk_countryId', '=', 'items.Fk_countryId')
+                ->leftJoin('associate', 'associate.Pk_assocId', '=', 'inventories.Fk_assocId')
+                ->leftJoin('location', 'location.Pk_locationId', '=', 'associate.Fk_locationId')
+                ->select('*', DB::raw('FORMAT(cost,2) as costs'), DB::raw('Quantity * pack_size + loose as qty'))
+                ->where('Pk_inventoryId', $req->inId)
+                ->get();
 
-    //         return response()->json($items);
-
-    //     } catch (\Throwable $th){
-    //         return response()->json([
-    //             'message' => $th
-    //         ]);
-    //     }
-    // }
+            return response()->json($detail);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => $th
+            ]);
+        }
+    }
 }
