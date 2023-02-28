b7cd9a8da11729581ffbbe4d4b7717cdb5a52888
dennis falcasantos <dennisfalcasantosnatividad@gmail.com>
2023-02-28 09:50:40 +0800

    sample upload

--- app/Http/Controllers/Helper/MessedUpController.php
+++ app/Http/Controllers/Helper/MessedUpController.php
@@ -89,10 +89,10 @@ class MessedUpController extends Controller
                     $details=get_object_vars($dataa)["details2"];
                     $other=get_object_vars($dataa)["other"];
                     //checkif props exist and stop
-                    //status
-                    $checkStatus=DB::table('status')
-                    ->select()
-                    ->where('status_name', '=', get_object_vars($dataa)["Status"])->first();
+                    // //status
+                    // $checkStatus=DB::table('status')
+                    // ->select()
+                    // ->where('status_name', '=', get_object_vars($dataa)["Status"])->first();
                     //brand
                     //manufacturer
                     //country
@@ -248,8 +248,8 @@ class MessedUpController extends Controller
             }
             }catch (\Throwable $th) {
 DB::rollBack();
+return $th;
             return response()->json([
-                
                 'status' => 500,
                 'message' => $th -> getMessage(),
             ]);
