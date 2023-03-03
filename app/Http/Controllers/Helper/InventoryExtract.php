0660dc84ed0fbdb1950021e03a8a76d38f35d362
dennis falcasantos <dennisfalcasantosnatividad@gmail.com>
2023-03-03 10:42:14 +0800

    temp

--- app/Http/Controllers/Helper/InventoryExtract.php
+++ app/Http/Controllers/Helper/InventoryExtract.php
@@ -35,12 +35,32 @@ class InventoryExtract extends Controller
 
              fclose($handle);
             for($i = 0 ; $i <count($data); $i++){
-                dd($data[$i]);
+                $dataa = DB::select("
+                        SELECT Pk_itemId,items.*, CONCAT(
+                        REPLACE(COALESCE(articles.article_name, ''), ' ', ''),
+                        REPLACE(COALESCE(types.type_name, ''), ' ', ''),
+                        REPLACE(COALESCE(items.model, ''), ' ', ''),
+                        REPLACE(COALESCE(variety.variety, ''), ' ', ''),
+                        REPLACE(COALESCE(items.details2, ''), ' ', ''),
+                        REPLACE(COALESCE(items.other, ''), ' ', '')
+                        ) as Concatvalue
+                        FROM `items`
+                        LEFT JOIN types ON Fk_typeId = types.Pk_typeId 
+                        LEFT JOIN articles ON types.Fk_articleId = articles.Pk_articleId 
+                        LEFT JOIN variety ON items.Fk_varietyId = variety.Pk_varietyId 
+                        HAVING Concatvalue = ?;
+                    ",[str_replace(" ","",$data[$i]["Item Description"])]);
+                    $dataa = $dataa[0]||null;
+                if($dataa){
+                    echo ('column no: ' . $i+2 . $data[$i]["Item Description"] . "   " ."PK IN DB is: ". get_object_vars($dataa)["Pk_itemId"] . "|||");
+                }
             }
 
             fclose($handle);
             DB::commit();
         }catch (\Throwable $th) {
+            echo("ERRROR at column".$i+2);
+            dd($dataa);
             DB::rollBack();
             return response()->json([
                 'status' => 500,
