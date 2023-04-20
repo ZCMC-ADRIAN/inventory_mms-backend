0b25a14a43f6e6a3c7bc548450277c336ffec22a
ZCMC-ADRIAN <adrian.zcmc@gmail.com>
2023-04-20 16:07:42 +0800

    April 20

--- app/Http/Controllers/EditLocation.php
+++ app/Http/Controllers/EditLocation.php
@@ -20,8 +20,8 @@ class EditLocation extends Controller
             DB::beginTransaction();
             $data = request()->all();
             $condition_id = $data['condition_id'];
-            $location_id = $data['location_id'];
-            $assoc_id = $data['assoc_id'];
+            $location_id = isset($data['location_id']) ? $data['location_id'] : null;
+            $assoc_id = isset($data['assoc_id']) ? $data['assoc_id'] : null;
             $newcondition_name = $data['newcondition_name'];
             $newlocation_name = $data['newlocation_name'];
             $newAssoc_name = $data['newAssoc_name'];
@@ -29,24 +29,42 @@ class EditLocation extends Controller
             $serial = $data['serial'];
             $delivery_date = $data['delivery_date'];
             $remarks = $data['remarks'];
+            
 
             $currData = DB::select('SELECT * FROM inventories LEFT JOIN locat_man ON inventories.Fk_locatmanId = locat_man.Pk_locatmanId WHERE Pk_inventoryId = ?', ["$req->inventoryId"]);
 
             $con_name = DB::table('conditions')->where('conditions_name', $newcondition_name)->count();
-            $loc_name = DB::table('location')->where('location_name', $newlocation_name)->count();
-            $assoc_name = DB::table('associate')->where('person_name', $newAssoc_name)->count();
 
-            // foreach($currData as $data){
-            //     $condId = $data->Fk_conditionsId;
-            //     $locatId = $data->Fk_locatmanId;
-            //     $date = $data->Delivery_date;
-            //     $property = $data->property_no;
-            //     $serNum = $data->serial;
-            //     $rem = $data->Remarks;
-            // }
+            if(!$remarks){
+                foreach($currData as $det){
+                    $getRemarks = $det->Remarks;
+                }
+                $remarks = $getRemarks;
+            }
+
+            if(!$delivery_date){
+                foreach($currData as $det){
+                    $getDelivery = $det->Delivery_date;
+                }
+                $delivery_date = $getDelivery;
+            }
+
+            if(!$prop_no){
+                foreach($currData as $det){
+                    $getProp = $det->property_no;
+                }
+                $prop_no = $getProp;
+            }
+
+            if(!$serial){
+                foreach($currData as $det){
+                    $getSerial = $det->serial;
+                }
+                $serial = $getSerial;
+            }
+            
 
             $isnew = false;
-
             //For Conditions
             if (!$condition_id) {
                 if ($con_name === 0) {
@@ -64,27 +82,42 @@ class EditLocation extends Controller
 
             //For Locations
             if (!$location_id) {
-                $loc = Location::create([
-                    'location_name' => $newlocation_name,
-                ]);
-                $location_id = $loc->id;
-                echo 'location selected is empty: created new loc';
-                $isnew = true;
+                if (!empty($newlocation_name)) {
+                    $loc = Location::create([
+                        'location_name' => $newlocation_name,
+                    ]);
+                    $location_id = $loc->id;
+                    echo 'location selected is empty: created new loc';
+                    $isnew = true;
+                } else {
+                    foreach ($currData as $data) {
+                        $locID = $data->Fk_locationId;
+                    }
+                    $location_id = $locID;
+                }
             }
 
 
             //For Associates
             if (!$assoc_id) {
-                $assoc = Associate::create([
-                    'person_name' => $newAssoc_name,
-                ]);
-                $assoc_id = $assoc->id;
-                echo 'assoc selected is empty: created new assoc';
-                $isnew = true;
+                if(!empty($newAssoc_name)){
+                    $assoc = Associate::create([
+                        'person_name' => $newAssoc_name,
+                    ]);
+                    $assoc_id = $assoc->id;
+                    echo 'assoc selected is empty: created new assoc';
+                    $isnew = true;
+                }else{
+                    foreach ($currData as $data) {
+                        $assocID = $data->Fk_assocId;
+                    }
+                    $assoc_id = $assocID;
+                }
             }
 
+
+
             if (!$isnew) {
-                echo 'find in db and try to get the id of the locatman';
                 $locatman = get_object_vars(DB::table('locat_man')
                     ->select()
                     ->where('Fk_assocId', '=', $assoc_id)
@@ -100,16 +133,18 @@ class EditLocation extends Controller
 
 
             DB::table('inventories')->where('Pk_inventoryId', $req->inventoryId)->update(['Fk_conditionsId' => $condition_id, 'Fk_locatmanId' => $locatman, 'Delivery_date' => $delivery_date, 'property_no' =>  $prop_no, 'serial' => $serial, 'Remarks' => $remarks]);
+            
 
             DB::commit();
             return response()->json([
                 "status" => 1
             ]);
+
         } catch (\Throwable $th) {
             DB::rollBack();
             return response()->json([
                 'status' => 500,
-                'message' => $th
+                'message' => $th->getMessage()
             ]);
         }
     }
