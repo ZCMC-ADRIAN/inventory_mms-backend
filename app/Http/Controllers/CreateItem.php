e9b8023c70d4d95a96f00fa7169cd9581983731b
Kim Horace A. Dolar <kimhorace@zcmcad.local>
2023-09-18 08:43:48 +0800

    kim commited

--- app/Http/Controllers/CreateItem.php
+++ app/Http/Controllers/CreateItem.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
+use App\Helpers\Helper;
+
 use App\Models\InsertArticle;
 use App\Models\InsertVariety;
 use App\Models\InsertBrand;
